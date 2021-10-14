250095d5dc9285f136aeb3b142191250e8fce91a
SaharAlassaf <Sahar.Hussain.elq.js@tuwaiq.edu.sa>
2021-10-14 12:29:40 +0300

    added files

---
+++ dataTypes.js
@@ -0,0 +1,33 @@
+/* Second Task */
+
+/* Integer */
+let number = 6;
+
+/* String */
+let name = "Sahar";
+
+/* Array */
+let arry = [1,2,3];
+
+/* Object */
+let obj = {
+    name : "Amal",
+    age : 22
+};
+
+console.log(number);
+console.log(name);
+console.log(arry);
+console.log(obj);
+
+console.log(typeof "Hi"); //string
+
+let arr = ["Rahaf",20, "Yazzid", 21];
+console.log(typeof arr); // object
+
+/* null variable  */
+let nullValue1 = null;
+let nullValue2 = undefined;
+
+/* undefined variable */
+let undefinedValue;
---
+++ myVariables.js
@@ -0,0 +1,17 @@
+/* First Task */
+
+let language = "JavaScript";
+let score = 10;
+console.log(language + score);
+
+
+let length = 3;
+let width = 3;
+console.log("The area of the rectangle is " + length * width);
+
+
+let num1 = 7;
+let num2 = 3;
+console.log(num1%num2);
+
+console.log(`${num1} / ${num2} = ` + Math.trunc(num1/num2) + " remainder "  + Math.trunc(num1%num2));
